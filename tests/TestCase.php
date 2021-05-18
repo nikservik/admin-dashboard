8f2062b7603790ba8f28a354e7f55f33d7ffa4af
Sergey Nikiforov <ser.nikiforov@gmail.com>
2021-05-18 19:39:20 +0200

    test and branch-alias

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,31 +20,18 @@ class TestCase extends Orchestra
             AdminDashboardServiceProvider::class,
             UsersServiceProvider::class,
             ActionServiceProvider::class,
-            SessionServiceProvider::class,
-            AuthServiceProvider::class,
-            ViewServiceProvider::class,
         ];
     }
 
     public function getEnvironmentSetUp($app)
     {
         $app['config']->set('app.locale', 'ru');
-        $app['config']->set('database.default', 'sqlite');
-        $app['config']->set('database.connections.sqlite', [
-            'driver' => 'sqlite',
-            'database' => ':memory:',
-            'prefix' => '',
-        ]);
-//        $app['config']->set('session.driver', 'array');
-//        $app['config']->set('session.store', 'array');
-//        $app['config']->set('cache.default', 'array');
+        $app['config']->set('database.default', 'mysql');
     }
 
     protected function defineDatabaseMigrations()
     {
         $this->loadLaravelMigrations();
-
-        include_once __DIR__.'/../vendor/nikservik/users/database/migrations/update_users_table_with_rights.php.stub';
-        (new \UpdateUsersTableWithRights)->up();
+        $this->loadMigrationsFrom(__DIR__ . '/../vendor/nikservik/users/database/migrations');
     }
 }
